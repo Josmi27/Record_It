7ab4b885d7ee9201293a9bb8eb111b4c8562ac4b
Joshua <jds13245@hotmail.com>
2019-06-08 10:42:22 -0400

    Created DatabaseHelper File and Partial Updates to Main Java File

--- app/src/main/java/me/josmi/thankyou/MainActivity.java
+++ app/src/main/java/me/josmi/thankyou/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,46 +14,60 @@ import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    // tag, database helper, edit text
+    private static final String TAG = "MainActivity";
+
+    DatabaseHelper mDatabaseHelper;
+    private Button Thankyou_Button;
+    private EditText editText;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Date Code
         Calendar calendar = Calendar.getInstance();
         String currentDate = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
 
         TextView textViewDate = findViewById(R.id.dateText);
         textViewDate.setText(currentDate);
 
+            // Button Code
         Button Thankyoubutton = findViewById(R.id.Thankyou_Button);
 
-//        Thankyoubutton.setOnClickListener();
         Thankyoubutton.setOnClickListener(this);
-//        Thankyoubutton.setEnabled(false);
 
 
+        // editText Code
+        EditText editText = findViewById(R.id.editText);
+
+        // Database Code
+        mDatabaseHelper = new DatabaseHelper(this);
+
+
+    }
+
+    public void AddData(String newEntry) {
+        // this is a boolean, because a bool was returned in the addData method
+        boolean insertData = mDatabaseHelper.addData(newEntry);
+
+        if (insertData) {
+            toastMessage("Data Successfully Inserted!");
+        } else {
+            toastMessage("Something went wrong");
+        }
+    }
+
+    private void toastMessage(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onClick(View v) {
         Toast toasty = Toast.makeText(this, "The Lord Has Received Your Message", Toast.LENGTH_LONG);
-//        toasty.setMargin(0,0);
         toasty.show();
-
-
-
-
     }
-
-
-//    Date c = Calendar.getInstance().getTime();
-//    System.out.println("Current time =>" + c);
-//
-//    SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
-//    String formattedDate = df.format(c);
-//
-//    TextView dateTextView = (TextView)findViewById(R.id.dateText);
-//    dateTextView.setText(formattedDate);\\
-
-
 }
